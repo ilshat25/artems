8fcdddcfb1e27754e75662855c6e92c2d5866b34
Conar <galiev25@list.ru>
2020-12-19 13:04:38 +0300

    4

--- pt.py
+++ pt.py
@@ -1,44 +1,45 @@
 t = int(input())
-s, n = (int(x) for x in input().split())
+for tt in range(t):
+    s, n = (int(x) for x in input().split())
 
-pr =  [[0]*n for i in range(n+1)]
-res = [[0]*n for i in range(n+1)]
-M = []
+    pr =  [[0]*n for i in range(n+1)]
+    res = [[0]*n for i in range(n+1)]
+    M = []
 
-for i in range(n):
-    M.append([int(x) for x in input().split()])
+    for i in range(n):
+        M.append([int(x) for x in input().split()])
 
-for i in range(1, n+1):
-    for k in range(n):
-        res[i][k] = res[i-1][k]
-        pr[i][k] = k
-    for j in range(n):
+    for i in range(1, n+1):
         for k in range(n):
-            if M[j][k] > 0 and res[i-1][j] + M[j][k] > res[i][k]:
-                idxt = j
-                flg = False
-                for c in range(i-1, 0, -1): 
-                    if idxt == k and pr[c][idxt] == j:
-                        flg = True
-                        break
-                    idxt = pr[c][idxt]
-                if flg: break
-                res[i][k] = res[i-1][j] + M[j][k]
-                pr[i][k] = j
+            res[i][k] = res[i-1][k]
+            pr[i][k] = k
+        for j in range(n):
+            for k in range(n):
+                if M[j][k] > 0 and res[i-1][j] + M[j][k] > res[i][k]:
+                    idxt = j
+                    flg = False
+                    for c in range(i-1, 0, -1): 
+                        if idxt == k and pr[c][idxt] == j:
+                            flg = True
+                            break
+                        idxt = pr[c][idxt]
+                    if flg: break
+                    res[i][k] = res[i-1][j] + M[j][k]
+                    pr[i][k] = j
 
-idx = 0
-for i in range(0, n):
-    idxt = i
-    if res[n][idxt] > res[n][idx]:
-        for j in range(n, 0, -1): idxt = pr[j][idxt]
-        if idxt == s - 1: idx = i
+    idx = 0
+    for i in range(0, n):
+        idxt = i
+        if res[n][idxt] > res[n][idx]:
+            for j in range(n, 0, -1): idxt = pr[j][idxt]
+            if idxt == s - 1: idx = i
 
-ans = []
-for i in range(n, 0, -1):
-    if pr[i][idx] != idx:
-        ans.append(M[pr[i][idx]][idx])
-    idx = pr[i][idx]
+    ans = []
+    for i in range(n, 0, -1):
+        if pr[i][idx] != idx:
+            ans.append(M[pr[i][idx]][idx])
+        idx = pr[i][idx]
 
-for el in reversed(ans):
-    print(el, end = ' ')
-print()
+    for el in reversed(ans):
+        print(el, end = ' ')
+    print()
