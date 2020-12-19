aecf2a07b6801294178e4b37411fd66cabab7dff
Conar <galiev25@list.ru>
2020-12-19 12:22:49 +0300

    2

--- pt.py
+++ pt.py
@@ -9,6 +9,9 @@ for i in range(n):
     M.append([int(x) for x in input().split()])
 
 for i in range(1, n+1):
+    for k in range(n):
+        res[i][k] = res[i-1][k]
+        pr[i][k] = k
     for j in range(n):
         for k in range(n):
             if M[j][k] > 0 and res[i-1][j] + M[j][k] > res[i][k]:
@@ -20,11 +23,12 @@ for i in range(0, n):
     idxt = i
     if res[n][idxt] > res[n][idx]:
         for j in range(n, 0, -1): idxt = pr[j][idxt]
-        if idxt == s: idx = i
+        if idxt == s - 1: idx = i
 
 ans = []
 for i in range(n, 0, -1):
-    ans.append(M[pr[i][idx]][idx])
+    if pr[i][idx] != idx:
+        ans.append(M[pr[i][idx]][idx])
     idx = pr[i][idx]
 
 for el in reversed(ans):
